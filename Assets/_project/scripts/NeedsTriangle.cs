969c2e42d7b0de7aaf5acf52f863e4fc004bae23
Michael Vaganov <michael.vaganov@gmail.com>
2020-01-31 20:47:04 -0800

    needs triangle

--- Assets/_project/scripts/NeedsTriangle.cs
+++ Assets/_project/scripts/NeedsTriangle.cs
@@ -5,34 +5,63 @@ using UnityEngine.UI;
 
 public class NeedsTriangle : MonoBehaviour
 {
-	public NeedLevel[] triangleLayers;
+	public NeedUI[] triangleLayers;
 
 	[System.Serializable]
 	public class Need {
-		public Color color;
 		public string name;
+		public Color color;
 		public float value;
 		public float lossPerSecond;
+		public float gainPerClick;
+		public NeedUI ui;
+		public Need dependency;
+		public void Update(float deltaTime) {
+			value -= lossPerSecond * deltaTime;
+			if (value < 0) { value = 0; }
+			float max = dependency != null ? dependency.value : 1;
+			if (value > max) { value = max; }
+			ui.progressbar.fillAmount = Mathf.Clamp(value, 0, 1);
+		}
+		public void Click() {
+			value += gainPerClick;
+		}
+		public void Use(NeedUI ui, Need dependency) {
+			this.dependency = dependency;
+			this.ui = ui;
+			ui.text.text = name;
+			ui.progressbar.color = color;
+		}
 	}
 
 	public Need[] needs = new Need[] {
-		new Need{color = Color.red, name = "pizza", value = .5f, lossPerSecond = 0.02f },
-		new Need{color = Color.red, name = "job", value = .5f, lossPerSecond = 0.04f },
-		new Need{color = Color.red, name = "friends", value = .5f, lossPerSecond = 0.06f },
-		new Need{color = Color.red, name = "feelin' swell", value = .5f, lossPerSecond = 0.08f },
-		new Need{color = Color.red, name = "encouraging people to vote for reasonable problem solvers", value = .5f, lossPerSecond = 0.1f },
+		new Need{color = new Color(1.0f,0.0f,0.0f,0.5f), value = .5f, lossPerSecond = 0.010f, gainPerClick = .1f, name = "pizza" },
+		new Need{color = new Color(1.0f,0.7f,0.0f,0.5f), value = .5f, lossPerSecond = 0.008f, gainPerClick = .1f, name = "job" },
+		new Need{color = new Color(0.5f,1.0f,0.0f,0.5f), value = .5f, lossPerSecond = 0.006f, gainPerClick = .1f, name = "friends" },
+		new Need{color = new Color(0.0f,1.0f,1.0f,0.5f), value = .5f, lossPerSecond = 0.004f, gainPerClick = .1f, name = "feelin' swell" },
+		new Need{color = new Color(0.0f,0.5f,1.0f,0.5f), value = .5f, lossPerSecond = 0.002f, gainPerClick = .1f, name = "political change" },
 	};
-
-//	public GameObject
 
 	void Start()
     {
-        
-    }
+		if (triangleLayers == null || triangleLayers.Length == 0) {
+			triangleLayers = GetComponentsInChildren<NeedUI>();
+		}
+		if (needs.Length > triangleLayers.Length) { throw new System.Exception("need at least "+ needs.Length+" layers in the pyramid"); }
+		for(int i = 0; i < needs.Length; ++i)
+		{
+			needs[i].Use(triangleLayers[i], i != 0? needs[i-1] : null);
+		}
+		System.Array.ForEach(needs, (need) => {
+			need.ui.button.onClick.AddListener(() => {
+				//Debug.Log("clicked " + need.name);
+				need.Click();
+			});
+		});
+	}
 
-    // Update is called once per frame
-    void Update()
+	void Update()
     {
-        
+		System.Array.ForEach(needs, (need) => need.Update(Time.deltaTime));
     }
 }
